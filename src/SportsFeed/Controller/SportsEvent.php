aba574fd487bcf25e4d0d05881366313c29ee211
vagrant <vagrant@php-training>
2022-04-21 18:48:17 -0700

    abstract, super class, magic methods, and subclasses

--- src/SportsFeed/Controller/SportsEvent.php
+++ src/SportsFeed/Controller/SportsEvent.php
@@ -5,7 +5,14 @@
  */
 namespace SportsFeed\Controller;
 use DateTime;
-class SportsEvent
+
+//abstract class for an event
+abstract class Event {
+  public function getEventName() : string; 
+}
+
+//super class for a SportsEvent
+class SportsEvent extends Event
 {
   // constructor argument promotion
   // only in PHP 8.0 and above
@@ -17,10 +24,91 @@ class SportsEvent
     public string $eventCategory = '',
     public string $eventDateString,
     public string $eventGUID = '',
-    public string $eventScore = '',
     public string $eventOpponent = ''
   )
   {
-        $this->eventDate = new DateTime($eventDateString);
+    $this->eventDate = new DateTime($eventDateString);
+  }
+  
+  public function getEventName(): string {
+    return 'Event Title'. $this->eventTitle;
+  };
+  
+  public function __serialize(): array {
+    return ["sportsEvent" => 'some new value'];
+  }
+  
+  public function __unserialize(array $data): void {
+    $this->fooValue = $data["fooValue"];
+    $this->barValue = 'barValue';
+  }
+  
+  public function __toString()
+  {
+     return __CLASS__;
   }
 }
+
+//subclass to create a score for past events
+class SportsScore extends SportsEvent {
+  public string $eventScore;
+  public function __construct(
+    string $eventTitle,
+    string $eventURL,
+    string $eventDescription,
+    string $eventCategory,
+    string $eventDateString,
+    string $eventGUID,
+    string $eventOpponent,
+    string $eventScore
+  )
+  {
+    parent::__construct(
+      $eventTitle, $eventURL, $eventDescription, $eventCategory, $eventDateString, $eventGUID, $eventOpponent);
+      $this->eventScore = $eventScore;
+    )
+  }
+}
+
+//subclass to create a score for future events
+class FutureEvent extends SportsEvent {
+  public string $eventSeed
+  public function __construct(
+    string $eventTitle,
+    string $eventURL,
+    string $eventDescription,
+    string $eventCategory,
+    string $eventDateString,
+    string $eventGUID,
+    string $eventOpponent,
+    string $eventScore
+  )
+  {
+    parent::__construct(
+      $eventTitle, $eventURL, $eventDescription, $eventCategory, $eventDateString, $eventGUID, $eventOpponent);
+      $this->eventSeed = $eventSeed;
+    )
+  }
+}
+//subclass for baseball
+class BaseballEvent extends SportsEvent {
+  public string $inningsPlayed;
+  public string $homeRuns;
+  public function __construct(
+    string $eventTitle,
+    string $eventURL,
+    string $eventDescription,
+    string $eventCategory,
+    string $eventDateString,
+    string $eventGUID,
+    string $eventOpponent,
+  )
+  {
+    parent::__construct(
+      $eventTitle, $eventURL, $eventDescription, $eventCategory, $eventDateString, $eventGUID, $eventOpponent);
+      $this->inningsPlayed = $inningsPlayed;
+      $this->homeRuns = $homeRuns;
+    )
+  }
+}
+
