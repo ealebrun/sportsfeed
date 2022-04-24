e757db8b286aaebc3d827f028a9efbdc69570eee
ealebrun <103968716+ealebrun@users.noreply.github.com>
2022-04-24 14:50:51 -0400

    Update SportsScore.php

--- src/SportsFeed/Controller/SportsScore.php
+++ src/SportsFeed/Controller/SportsScore.php
@@ -10,16 +10,6 @@ use DateTime;
 //subclass to create a score for past events
 class SportsScore extends SportsEvent {
   public string $eventScore;
-  public function __construct(
-    string $eventTitle,
-    string $eventURL,
-    string $eventDescription,
-    string $eventCategory,
-    string $eventDateString,
-    string $eventGUID,
-    string $eventOpponent,
-    string $eventScore
-  )
   {
     parent::__construct($eventTitle, $eventURL, $eventDescription, $eventCategory, $eventDateString, $eventGUID, $eventOpponent);
       $this->eventScore = $eventScore;
