fcda06b7ca1d60b980a2a1625b2e744073042782
ealebrun <103968716+ealebrun@users.noreply.github.com>
2022-04-24 14:51:17 -0400

    Update FutureEvent.php

--- src/SportsFeed/Controller/FutureEvent.php
+++ src/SportsFeed/Controller/FutureEvent.php
@@ -9,16 +9,6 @@ use DateTime;
 //subclass to create a score for future events
 class FutureEvent extends SportsEvent {
   public string $eventSeed;
-  public function __construct(
-    string $eventTitle,
-    string $eventURL,
-    string $eventDescription,
-    string $eventCategory,
-    string $eventDateString,
-    string $eventGUID,
-    string $eventOpponent,
-    string $eventScore
-  )
   {
     parent::__construct($eventTitle, $eventURL, $eventDescription, $eventCategory, $eventDateString, $eventGUID, $eventOpponent);
       $this->eventSeed = $eventSeed;
